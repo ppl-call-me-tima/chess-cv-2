de2d9da71b7e460a30b3be4ab7eb20528bf546c0
Amit Upadhyay <2648amitupadhyay@gmail.com>
2025-12-25 19:08:08 +0530

    Refractor: Add camera manager and move camera functionality is setup screen to it

---
+++ managers/camera_manager.py
@@ -0,0 +1,44 @@
+import cv2
+from cv2_enumerate_cameras import enumerate_cameras
+
+from helpers.misc import cv2pygame
+
+class CameraManager:
+    def __init__(self):
+        self.cap = None
+        self.index = None
+        self.indices_set = set()
+    
+    def set_index(self, index):
+        self.index = index
+
+    def get_camera_list(self):
+        self.camera_list = [(cam.index, cam.name) for cam in enumerate_cameras(cv2.CAP_MSMF)]
+        return self.camera_list
+
+    def set_camera(self, index):
+        indices_set = set()
+        for ind, _ in self.camera_list:
+            indices_set.add(ind)
+
+        if index in indices_set:
+            if self.index == index and self.cap is not None and self.cap.isOpened():
+                return True
+
+            if self.cap is not None:
+                self.cap.release()
+            
+            self.cap = cv2.VideoCapture(index=index, apiPreference=cv2.CAP_MSMF)
+            if self.cap.isOpened():
+                self.index = index
+                return True
+        
+        return False
+
+    def get_frame(self):
+        if self.cap is not None and self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if ret:
+                return cv2pygame(frame)
+        
+        return None
--- screens/setup_screen.py
+++ screens/setup_screen.py
@@ -1,9 +1,9 @@
 import cv2
-from cv2_enumerate_cameras import enumerate_cameras
 import pygame
 
-from ui_components.dropdown import Dropdown
 from screens.base_screen import BaseScreen
+from ui_components.dropdown import Dropdown
+from managers.camera_manager import CameraManager
 
 class SetupScreen(BaseScreen):
     def __init__(self, manager):
@@ -11,7 +11,8 @@ class SetupScreen(BaseScreen):
         self.font = pygame.font.SysFont("Arial", 25)
         self.font_colour = pygame.Color(255, 255, 255)
         
-        self.cameras = [(cam.index, cam.name) for cam in enumerate_cameras(cv2.CAP_MSMF)]
+        self.camera_manager = CameraManager()
+        self.cameras = self.camera_manager.get_camera_list()
         
         self.labels = [
             {"text": "Select Camera:", "rect": pygame.Rect(50, 170, 400, 30)},
@@ -32,14 +33,40 @@ class SetupScreen(BaseScreen):
         )
 
         self.feed_surf = None
+        self.feed_rect = pygame.Rect(500, 50, 730, 620)
+        self.feed_text = "No camera selected"
 
     async def handle_event(self, event):
-        selected_camera_index = self.camera_dropdown.handle_event(event)
-        selected_gpu_index = self.gpu_dropdown.handle_event(event)
+        camera_index = self.camera_dropdown.handle_event(event)
+        gpu_index = self.gpu_dropdown.handle_event(event)
+
+        if camera_index is not None:
+            self.camera_manager.set_camera(camera_index)
+
+    def update(self):
+        if self.camera_manager.cap is None:
+            return
+
+        frame = self.camera_manager.get_frame()
+        if frame is not None:
+            self.feed_surf = pygame.surfarray.make_surface(frame)
+            self.feed_surf = pygame.transform.scale(self.feed_surf, (self.feed_rect.width, self.feed_rect.height))
+        else:
+            self.feed_surf = None
+            self.feed_text = "Camera not available"
 
     def draw(self, surface):
+        for label in self.labels:
+            surface.blit(self.font.render(label["text"], True, self.font_colour), label["rect"])
+        
         self.camera_dropdown.draw(surface)
         self.gpu_dropdown.draw(surface)
 
-        for label in self.labels:
-            surface.blit(self.font.render(label["text"], True, self.font_colour), label["rect"])
+        pygame.draw.rect(surface, (255, 255, 255), self.feed_rect, 1)
+
+        if self.feed_surf:
+            surface.blit(self.feed_surf, self.feed_rect)
+        else:
+            text_surf = self.font.render(self.feed_text, True, (100, 100, 100))
+            text_rect = text_surf.get_rect(center=self.feed_rect.center)
+            surface.blit(text_surf, text_rect)
