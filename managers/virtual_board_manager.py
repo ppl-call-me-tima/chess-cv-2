8d16bffc6f72aed12746f90c991ff8acdcc236e6
Amit Upadhyay <2648amitupadhyay@gmail.com>
2026-05-20 15:11:53 +0530

    add infinite piece selector pallette + move held piece mechanism to piece storing instead of coordinate storing

--- managers/virtual_board_manager.py
+++ managers/virtual_board_manager.py
@@ -6,13 +6,23 @@ class VirtualBoard:
     def __init__(self, feed_rect: pygame.Rect):
         self.is_enabled = True
 
-        self.TOPLEFT = feed_rect.topleft
+        self.LEFT = feed_rect.left
+        self.TOP = feed_rect.top
+        self.HEIGHT = feed_rect.height
         self.SQ_SIZE = feed_rect.height // 8
+        self.SELECTOR_SQ_SIZE = int(0.75 * self.SQ_SIZE)
 
-        self.matrix = [[""] * 8 for i in range(8)]      # 8x8 matrix respresenting board
-        self.state_matrix = {}                          # dict of {cood: pygame.Rect}
-        self.held_piece_cood = None                     # (r, c) of chessboard sq of piece held / None
-        self.piece_images = self.__load_sprite_sheet()  # dict of {"k/q/n/b/r/p": pygame.Surface (piece image)}
+        self.board_matrix = [[""] * 8 for i in range(8)]         # 8x8 matrix respresenting board
+        self.board_state_matrix = {}                             # dict of {cood: pygame.Rect} for chessboard
+        self.selector_state_matrix = {}                          # dict of {piece: pygame.Rect} for selector pallette
+        self.held_piece = {"piece": None, "is_infinite": False}
+
+        self.selector_matrix = (
+            ("K", "Q", "B", "N", "R", "P"),
+            ("k", "q", "b", "n", "r", "p"),
+        )
+
+        self.piece_images, self.selector_images = self.__load_sprite_sheet()  # dict of {piece: pygame.Surface (piece/selector image)}
 
     def __load_sprite_sheet(self):
         sheet = pygame.image.load(PIECES_SPRITE_SHEET_PATH).convert_alpha()
@@ -26,48 +36,63 @@ class VirtualBoard:
             "k": (0, 1), "q": (1, 1), "b": (2, 1), "n": (3, 1), "r": (4, 1), "p": (5, 1),
         }
 
-        images = {}
+        piece_images = {}
+        selector_images = {}
 
         for name, pos in piece_map.items():
             rect = pygame.Rect(pos[0] * piece_width, pos[1] * piece_height, piece_width, piece_height)
             piece_image = sheet.subsurface(rect)
-            images[name] = pygame.transform.scale(piece_image, (self.SQ_SIZE, self.SQ_SIZE))
-        
-        return images
+            piece_images[name] = pygame.transform.scale(piece_image, (self.SQ_SIZE, self.SQ_SIZE))
+            selector_images[name] = pygame.transform.scale(piece_image, (self.SELECTOR_SQ_SIZE, self.SELECTOR_SQ_SIZE))
+
+        return piece_images, selector_images
 
     def handle_event(self, event: pygame.event.Event):
-        # move making
+        # move making and infinite piece selecting
         if event.type == pygame.MOUSEBUTTONDOWN:
-            for cood, rect in self.state_matrix.items():
+            for cood, rect in self.board_state_matrix.items():
                 mouse_pos = event.pos
                 if rect.collidepoint(mouse_pos):
-                    if self.held_piece_cood is None:
-                        if self.matrix[cood[0]][cood[1]] != "":
-                            self.held_piece_cood = cood
+                    if self.held_piece["piece"] is None and not self.held_piece["is_infinite"]:
+                        if self.board_matrix[cood[0]][cood[1]] != "":
+                            self.held_piece["piece"] = self.board_matrix[cood[0]][cood[1]]
+                            self.board_matrix[cood[0]][cood[1]] = ""
                     else:
-                        r, c = self.held_piece_cood
-                        piece = self.matrix[r][c]
-                        
-                        self.matrix[r][c] = ""
-                        self.matrix[cood[0]][cood[1]] = piece
-                        self.held_piece_cood = None
+                        self.board_matrix[cood[0]][cood[1]] = self.held_piece["piece"]
+                        self.held_piece["piece"] = None
+                        self.held_piece["is_infinite"] = False
+
+            for piece, rect in self.selector_state_matrix.items():
+                mouse_pos = event.pos
+                if rect.collidepoint(mouse_pos):
+                    self.held_piece["piece"] = piece
+                    self.held_piece["is_infinite"] = True
 
     def draw_board(self, surface: pygame.Surface):
         # board
         for r in range(8):
             for c in range(8):
                 colour = (60, 60, 60) if (r + c) % 2 else (245, 245, 245)
-                x = self.TOPLEFT[0] + c * self.SQ_SIZE + 1
-                y = self.TOPLEFT[1] + r * self.SQ_SIZE + 1
+                x = self.LEFT + c * self.SQ_SIZE + 1
+                y = self.TOP + r * self.SQ_SIZE + 1
                 rect = pygame.Rect(x, y, self.SQ_SIZE, self.SQ_SIZE)
-                self.state_matrix[(r, c)] = rect
+                self.board_state_matrix[(r, c)] = rect
                 pygame.draw.rect(surface, colour, rect)
 
-                if self.matrix[r][c] != "":
-                    surface.blit(self.piece_images[self.matrix[r][c]], (x, y))
+                if self.board_matrix[r][c] != "":
+                    surface.blit(self.piece_images[self.board_matrix[r][c]], (x, y))
 
         # sq highlighting
         mouse_pos = pygame.mouse.get_pos()
-        for rect in self.state_matrix.values():
+        for rect in self.board_state_matrix.values():
             if rect.collidepoint(mouse_pos):
                 pygame.draw.rect(surface, (255, 0, 0), rect, 2)
+
+        # infinite piece selector pallette
+        for c in range(2):
+            for r in range(6):
+                x = self.LEFT + self.HEIGHT + c * self.SELECTOR_SQ_SIZE
+                y = self.TOP + r * self.SELECTOR_SQ_SIZE
+                rect = pygame.Rect(x, y, self.SELECTOR_SQ_SIZE, self.SELECTOR_SQ_SIZE)
+                self.selector_state_matrix[self.selector_matrix[c][r]] = rect
+                surface.blit(self.selector_images[self.selector_matrix[c][r]], (x, y))
